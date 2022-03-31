ed50a7a7d76ccbd5f9fc87f15b5baf9990415224
kylamaisse19 <102754097+kylamaisse19@users.noreply.github.com>
2022-04-01 02:20:51 +0800

    Add files via upload

---
+++ git-python-marciano.py
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Personal Info</title>
+</head>
+<body>
+    print("Kyla Marisse R. Marciano")
+    print("Poctoy Torrijos Marinduque")
+    print("July 3, 2001")
+    print("Marinduque State College")
+    print("Bachelor of Science in Information Technology")
+    print("Second Year, Section E")
+    print("Sleeping")
+    print("Organizing my room")
+    print("Eating")
+    print("Watching Kdrama and English movie")
+    print("Swimming")
+</body>
+</html>
